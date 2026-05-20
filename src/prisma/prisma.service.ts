f13c0702275806bcbe30dede0458b9731f5e825c
Bestin John <bestinjohn1993@gmail.com>
2026-05-20 22:24:17 +0200

    feat(prisma): refactor PrismaService constructor to use ConfigService for DATABASE_URL

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaPg } from '@prisma/adapter-pg';
+import { Env } from 'src/config/env.schema';
 import { PrismaClient } from '../generated/prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(private configService: ConfigService<Env>) {
     const adapter = new PrismaPg({
-      connectionString: process.env.DATABASE_URL as string,
+      connectionString: configService.get<string>('DATABASE_URL'),
     });
     super({ adapter });
   }
