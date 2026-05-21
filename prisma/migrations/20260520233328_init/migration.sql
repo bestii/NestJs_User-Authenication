02844b856498d0bfc855828ed30edba020137ccd
Bestin John <bestinjohn1993@gmail.com>
2026-05-21 09:29:01 +0200

    feat(prisma): create initial User table with required fields and primary key

--- prisma/migrations/20260520233328_init/migration.sql
+++ prisma/migrations/20260520233328_init/migration.sql
@@ -1,7 +1,7 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT,
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
     "username" TEXT NOT NULL,
     "password" TEXT NOT NULL,
 
