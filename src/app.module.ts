85ae3b67894ae9167d9c4d655361750efcfd771f
Bestin John <bestinjohn1993@gmail.com>
2026-05-25 20:13:48 +0200

    refactor(config): remove unused configuration files and simplify app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import appConfig from './config/app.config';
-import authConfig from './config/auth.config';
-import databaseConfig from './config/database.config';
 import { validateEnv } from './config/validate-env';
 import { HealthModule } from './health/health.module';
-import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -19,7 +16,6 @@ import { PrismaModule } from './prisma/prisma.module';
       cache: true,
       envFilePath: ['.env'],
       validate: validateEnv,
-      load: [appConfig, databaseConfig, authConfig],
     }),
     UsersModule,
     AuthModule,
--- src/config/auth.config.ts
+++
@@ -1,6 +0,0 @@
-import { registerAs } from '@nestjs/config';
-
-export default registerAs('auth', () => ({
-  jwtSecret: process.env.JWT_SECRET,
-  expiresIn: process.env.JWT_EXPIRES_IN,
-}));
